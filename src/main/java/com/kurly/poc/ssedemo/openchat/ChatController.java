eeb7d9a2bb13292a77990ee2d1584d3cdbfc4164
JohnGrib <johngrib.lee@kurlycorp.com>
2019-11-20 18:06:14 +0900

    ChatController가 CheckStyle을 따르게 하라

--- src/main/java/com/kurly/poc/ssedemo/openchat/ChatController.java
+++ src/main/java/com/kurly/poc/ssedemo/openchat/ChatController.java
@@ -1,6 +1,11 @@
 package com.kurly.poc.ssedemo.openchat;
 
 import com.kurly.poc.ssedemo.common.Message;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.MediaType;
@@ -11,10 +16,6 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
-import java.io.IOException;
-import java.util.List;
-import java.util.concurrent.CopyOnWriteArrayList;
-
 @RestController
 @RequestMapping("/chat")
 public class ChatController {
@@ -22,6 +23,9 @@ public class ChatController {
   private final Logger logger = LoggerFactory.getLogger(ChatController.class);
   private final List<SseEmitter> emitters = new CopyOnWriteArrayList<>();
 
+  /**
+   * 채팅 참여자를 받는다.
+   */
   @GetMapping("/connect")
   public SseEmitter connect() {
     logger.info("/connect");
@@ -34,6 +38,12 @@ public class ChatController {
     return emitter;
   }
 
+  /**
+   * 채팅 참여자들에게 메시지를 전달한다.
+   *
+   * @param message 전달할 메시지
+   * @return 전달한 메시지
+   */
   @ResponseBody
   @PostMapping("/send")
   public Message broadcast(ChatMessage message) {
